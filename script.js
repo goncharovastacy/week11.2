99fac74466fa7e80ac96230991c821ab1ace06f8
goncharovastacy <gggnchr@gmail.com>
2023-04-01 11:01:28 +0300

    1

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('button');
+function calculateSquare (){
+    let number = document.getElementById('input1');
+    let number2 = number.value;
+    console.log(number2);
+}
+button.addEventListener('click', calculateSquare);
